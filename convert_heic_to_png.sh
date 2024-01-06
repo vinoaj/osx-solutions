dbf49b8bc26677196f48c69b50841537dbb7b274
Vinoaj (Vinny) Vijeyakumaar <vinoaj@gmail.com>
2024-01-07 10:15:12 +1100

    update

--- convert_heic_to_png.sh
+++ convert_heic_to_png.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+# Set the directory to the first argument, or default to the current directory if not provided
+DIR=${1:-.}
+
+# Change to the specified directory
+cd "$DIR"
+
+# Check if the directory change was successful
+if [ $? -ne 0 ]; then
+    echo "Cannot change to directory $DIR."
+    exit 1
+fi
+
+# Change file extension to lowercase
 for f in *.HEIC
-do sips -s format png "${f}" --out "${f%.*}.png"
+do 
+    mv "$f" "${f%.HEIC}.heic"
+done
+
+# Convert HEIC files to PNG and then delete the HEIC files
+for f in *.heic
+do 
+    sips -s format png "${f}" --out "${f%.*}.png"
+    rm "$f"
 done
